772993f29c5b1c325cb347e0815a4f3d150c0ad8
Pedro <Pedrogor@outlook.com>
2024-03-16 22:45:16 -0300

    JWT configs

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -1,45 +1,54 @@
-import * as Yup from 'yup';
-import User from '../models/User';
+import * as Yup from 'yup'
+import Jwt from 'jsonwebtoken'
+import User from '../models/User'
+import authConfig from '../../config/auth'
 
 class SessionController {
-    async store(request, response) {
-        const schema = Yup.object().shape({
-            email: Yup.string().email().required(),
-            password: Yup.string().required(),
-        });
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    })
 
-        const userEmailOrPassordIncorrect = () => {
-            return response.status(400).json({ error: "Make sure your password and email are correct" });
-
-        }
-
-        if (!(await schema.isValid(request.body))) {
-            userEmailOrPassordIncorrect()
-        }
-
-        const { email, password } = request.body;
-
-        try {
-            const user = await User.findOne({
-                where: { email }
-            });
-
-            if (!user) {
-                userEmailOrPassordIncorrect()
-
-            }
-
-            if (!(await user.checkPassword(password))) {
-                userEmailOrPassordIncorrect()
-
-            }
-
-            return response.json({ id: user.id, email, name: user.name, admin: user.admin });
-        } catch (error) {
-            console.error(error);
-            return response.status(500).json({ error: "Internal server error" });
-        }
+    const userEmailOrPassordIncorrect = () => {
+      return response
+        .status(400)
+        .json({ error: 'Make sure your password and email are correct' })
     }
+
+    if (!(await schema.isValid(request.body))) {
+      userEmailOrPassordIncorrect()
+    }
+
+    const { email, password } = request.body
+
+    try {
+      const user = await User.findOne({
+        where: { email },
+      })
+
+      if (!user) {
+        userEmailOrPassordIncorrect()
+      }
+
+      if (!(await user.checkPassword(password))) {
+        userEmailOrPassordIncorrect()
+      }
+
+      return response.json({
+        id: user.id,
+        email,
+        name: user.name,
+        admin: user.admin,
+        token: Jwt.sign({ id: user.id }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn,
+        }),
+      })
+    } catch (error) {
+      console.error(error)
+      return response.status(500).json({ error: 'Internal server error' })
+    }
+  }
 }
 
-export default new SessionController();
+export default new SessionController()
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController'
 import SessionsController from './app/controllers/SessionsController'
 import ProductController from './app/controllers/ProductController'
+import authMiddleware from './app/middlewares/auth'
 
 const upload = multer(multerConfig)
 
@@ -14,7 +15,9 @@ routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionsController.store)
 
+routes.use(authMiddleware)
+
 routes.post('/products',upload.single('file'), ProductController.store)
-routes.get('/products', ProductController.index)
+routes.get('/products',ProductController.index)
 
 export default routes
