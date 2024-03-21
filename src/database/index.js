2f756b6dd9aaffe42db209afa83129ffa9e75491
Pedro <Pedrogor@outlook.com>
2024-03-21 19:58:37 -0300

    associateTables

--- src/database/index.js
+++ src/database/index.js
@@ -11,11 +11,16 @@ const models = [User, Product, Category]
 class Database {
   constructor() {
     this.init()
+    //this.mongo()
   }
 
   init() {
     this.connection = new Sequelize(configDatabase)
-    models.map((model) => model.init(this.connection))
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models),
+      )
   }
 }
 
